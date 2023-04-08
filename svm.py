e43f517fd8b8c1eada8d45968d35be15d4f45637
tonishka <tonishkasingh21@gmail.com>
2023-04-08 17:02:46 +0800

    Clean up

--- svm.py
+++ svm.py
@@ -1,27 +1,24 @@
 # Importing Libraries
 
-import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
-from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
 from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import classification_report
 
-#2 Importing the dataset
-train_csv = pd.read_csv('train_100.csv')
+# 2 Importing the dataset
+train_csv = pd.read_csv('train_pca_100.csv', header=None)
 
-#3 Training
-x_train = train_csv.iloc[:,:-1]
+# 3 Training
+x_train = train_csv.iloc[:, :-1]
 y_train = train_csv.iloc[:, -1]
-param_grid = {'C': [0.1,1, 10, 100], 'gamma': [1,0.1,0.01,0.001],'kernel': ['rbf', 'poly', 'sigmoid', 'linear']}
-grid = GridSearchCV(SVC(),param_grid,refit=True,verbose=2)
-grid.fit(x_train,y_train)
+param_grid = {'C': [0.1, 1, 10, 100], 'gamma': [1, 0.1, 0.01, 0.001], 'kernel': ['poly', 'rbf', 'linear', 'sigmoid']}
+grid = GridSearchCV(SVC(), param_grid, refit=True, verbose=2)
+grid.fit(x_train, y_train)
 
 print(grid.best_estimator_)
 
-test_data = pd.read_csv("test_100.csv")
-x = test_csv.iloc[:,:-1]
+test_csv = pd.read_csv("test_pca_100.csv", header=None)
+x_test = test_csv.iloc[:, :-1]
 y_test = test_csv.iloc[:, -1]
 grid_predictions = grid.predict(x_test)
-print(confusion_matrix(y_test,grid_predictions))
-print(classification_report(y_test,grid_predictions))
+print(classification_report(y_test, grid_predictions))
